c3fddd8dca051880940a64671bd455a1dbe33180
Patricia <patchley02@gmail.com>
2018-06-25 14:49:06 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,38 @@
 # Code your instances here
+class GardenGnome
+
+  def initialize(personality="evil")
+  @evil_personality = personality
+  puts "My gnome has an #{personality}."
+  end
+
+  def name=(g_name)
+   @name = g_name
+  end
+
+  def name
+   @name
+  end
+
+  def age=(g_age)
+  @age = g_age
+  end
+
+  def age
+  @age
+  end
+
+  def gluten_allergy=(g_allergy)
+  @gluten_allergy = g_allergy
+  end
+
+  def gluten_allergy
+  @gluten_allergy
+  end
+
+end
+
+title = GardenGnome.new(personality)
+title.name = "Steve"
+title.age = "4000"
+puts "My gnome's name is #{title.name}. He is #{title.age} years old."
